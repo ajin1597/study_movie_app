105d9a9abb6a2beaeabaed85093a74a34df4b64f
ajin15971@gmail.com <ajin15971@gmail.com>
2022-09-15 18:32:38 +0900

    20220915

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # STUDY_MOVIE_APP
 
+...
 git add .
-
 git commit -m "커밋메시지"
-
 git push
+...
---
+++ src/Food.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Food(props) {
+  return <div>나는 {props.fav}을 좋아합니다</div>;
+}
+
+export default Food;
